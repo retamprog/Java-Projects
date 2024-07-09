ba1e160b38d4a4ffbf8d98f61a3a2fc5beaf262f
retamprog <120020649+retamprog@users.noreply.github.com>
2024-07-09 21:15:30 +0530

    tenth commit

--- src/bank/management/system/Withdrawal.java
+++ src/bank/management/system/Withdrawal.java
@@ -4,20 +4,19 @@ package bank.management.system;
 /**
  *
  * @author RETAM
- * here we are going to create the deposit frame which is very similar to the Transactions frame
- * 
+ * here we will create the 
  */
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class Deposit extends JFrame implements ActionListener {
-     JTextField amount;
-     JButton deposit,back;
-     String pinnumber;
-     Deposit(String pinnumber){
+public class Withdrawal extends JFrame implements ActionListener{
+    JButton withdraw,back;
+    JTextField amount;
+    String pinnumber;
+    Withdrawal(String pinnumber){
          this.pinnumber=pinnumber;
-         //using the ImageIcon class to add images from the system to our JFrame
+           //using the ImageIcon class to add images from the system to our JFrame
            ImageIcon i1=new ImageIcon(ClassLoader.getSystemResource("icons/atm.jpg"));
            //now we will convert the ImageIcon object to a Image object inorder to scale it size and fit it the frame
            Image i2=i1.getImage().getScaledInstance(900,850,Image.SCALE_DEFAULT);
@@ -26,58 +25,56 @@ public class Deposit extends JFrame implements ActionListener {
            background.setBounds(0,0,900,850);
 //           add(background);
            add(background);
-           //adding the deposit label
-           JLabel label=new JLabel("Please Enter The Deposit Amount ");
+           //withdrawal amt label
+           JLabel withdrawalL=new JLabel("Maximum Withdrawal  is Rs 20,000");
+           withdrawalL.setBounds(170,260,450,30);
+           withdrawalL.setFont(new Font("Raleway",Font.BOLD,20));
+           withdrawalL.setForeground(Color.white);
+           background.add(withdrawalL);
+           JLabel label=new JLabel("Please Enter The Amount ");
            label.setFont(new Font("Raleway",Font.BOLD,18));
            label.setForeground(Color.white);
-           label.setBounds(180,280,320,20);
+           label.setBounds(215,300,320,20);
            background.add(label);
-           //now adding the depost amt text field
+           //the amount text field
            amount=new JTextField();
-           amount.setBounds(170,315,340,30);
+           amount.setBounds(170,340,340,25);
            amount.setFont(new Font("Raleway",Font.BOLD,18));
            background.add(amount);
-           //now adding the deposit button and back button
-           deposit=new JButton("Deposit");
-           deposit.setBounds(350,420,150,30);
-           deposit.setFont(new Font("Raleway",Font.BOLD,18));
-           deposit.addActionListener(this);
-           background.add(deposit);
-           back=new JButton("Back");
+           //now setting the buttons withdrawal and back
+           withdraw=new JButton("Withdrawal");
+           withdraw.setBounds(350,420,150,30);
+           withdraw.setFont(new Font("Raleway",Font.BOLD,18));
+           background.add(withdraw);
+           back=new JButton("back");
            back.setBounds(350,470,150,30);
            back.setFont(new Font("Raleway",Font.BOLD,18));
-           back.addActionListener(this);
            background.add(back);
+           
            setLayout(null);
            setSize(900,850);
 //           setUndecorated(true);
            setVisible(true);   
            setLocation(320,0);
            
-     }
-     
-    public void actionPerformed(ActionEvent ae){
-        if(ae.getSource()==deposit){
-            
-        }
-        else if(ae.getSource()==back){
-              setVisible(false);
-              new Transactions(pinnumber);
-                      
-        }
-        
     }
+   
     public static void main(String[] args) {
         
           try {
             //here you can put the selected theme class name in JTattoo
             UIManager.setLookAndFeel("com.jtattoo.plaf.aluminium.AluminiumLookAndFeel");
-            new Deposit("");
+            new Withdrawal("");
  
         } catch (Exception ex) {
             System.out.println(ex);
             
         } 
+        
+    }
+    public void actionPerformed(ActionEvent ae){
+        
+        
     }
     
     
